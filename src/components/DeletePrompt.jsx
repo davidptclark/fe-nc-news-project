4e05355350845563a7cf3aee4fac9bc071f82e59
David Clark <david.p.t.clark@gmail.com>
2022-04-07 18:55:43 +0100

    Add timer to alerts

--- src/components/DeletePrompt.jsx
+++ src/components/DeletePrompt.jsx
@@ -1,7 +1,19 @@
-import Alert from '@mui/material/Alert';
+import Alert from "@mui/material/Alert";
+import { useEffect } from "react";
 
 export default function SubmitPrompt({ deleteStatus, setDeleteStatus }) {
-  if (deleteStatus === 'success') {
+  useEffect(() => {
+    const timeId = setTimeout(() => {
+      // After 1.5 seconds set the show value to false
+      setDeleteStatus(false);
+    }, 1500);
+
+    return () => {
+      clearTimeout(timeId);
+    };
+  });
+
+  if (deleteStatus === "success") {
     return (
       <Alert
         className="delete-prompt"
@@ -13,7 +25,7 @@ export default function SubmitPrompt({ deleteStatus, setDeleteStatus }) {
         Comment successfully deleted!
       </Alert>
     );
-  } else if (deleteStatus === 'failure') {
+  } else if (deleteStatus === "failure") {
     return (
       <Alert
         className="delete-prompt"
