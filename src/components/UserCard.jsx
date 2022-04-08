a3a095955c0d54d24cac2b3a4013d0bc007cd3d0
David Clark <david.p.t.clark@gmail.com>
2022-04-08 09:39:27 +0100

    Add loading indicator for topic links and user card

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,15 +1,21 @@
 import * as api from "../utils/api";
 import { useEffect, useState } from "react";
 import Card from "@mui/material/Card";
+import CircularProgress from "@mui/material/CircularProgress";
 
 export default function UserCard() {
   const [userDetails, setUserDetails] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    api
-      .getUserDetailsByUsername("grumpy19")
-      .then(({ data: user }) => setUserDetails(user[0]));
+    setIsLoading(true);
+    api.getUserDetailsByUsername("grumpy19").then(({ data: user }) => {
+      setUserDetails(user[0]);
+      setIsLoading(false);
+    });
   }, []);
+
+  if (isLoading) return <CircularProgress />;
   return (
     <Card className="user-card" elevation={3} sx={{ width: 200 }}>
       <h4>{userDetails.username}</h4>
